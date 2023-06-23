883cda40ba3d3a8ffe3ec2099063d951fd247a00
Samrat Hossain <samratcse17019@gmail.com>
2023-06-23 19:24:39 +0600

    creatte booklist

--- Backend/Bookstore/Book/models.py
+++ Backend/Bookstore/Book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    bookname = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.bookname
--- Backend/Bookstore/Book/views.py
+++ Backend/Bookstore/Book/views.py
@@ -1,6 +1,12 @@
 
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from .serializers import BookSerializer
+from rest_framework.response import Response
+from .models import Book
 
+@api_view(['GET'])
 def BookList(request):
-    return HttpResponse("Atomic Habit")
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
 
