2dee9a4ab4aaf6920f8f16d0a7394adac72e0fe9
Samrat Hossain <samratcse17019@gmail.com>
2023-06-23 20:01:01 +0600

    Create Add Book API

--- Backend/Bookstore/Book/urls.py
+++ Backend/Bookstore/Book/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BookList, name="BookList")
+    path('', views.BookList, name="BookList"),
+    path('add-book', views.AddBook, name="AddBook")
 ]
--- Backend/Bookstore/Book/views.py
+++ Backend/Bookstore/Book/views.py
@@ -10,3 +10,12 @@ def BookList(request):
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
 
+@api_view(['POST'])
+def AddBook(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+    # return Response({"name":"Samrat"})
+
